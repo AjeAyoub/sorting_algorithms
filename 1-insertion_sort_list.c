b0b673ed26f96462e557df682bf9556935c3d044
AjeAyoub <ajeddabayoub.official@gmail.com>
2023-11-21 08:53:39 -0800

    algorithms

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,40 +7,40 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 
-    listint_t *current, *prev, *temp;
+	listint_t *current, *prev, *temp;
 
-    current = (*list)->next;
+	current = (*list)->next;
 
-    while (current)
-    {
-        prev = current->prev;
-        while (prev != NULL && prev->n > current->n)
-        {
-            temp = prev->prev;
+	while (current)
+	{
+		prev = current->prev;
+		while (prev != NULL && prev->n > current->n)
+		{
+			temp = prev->prev;
 
-            if (temp != NULL)
-                temp->next = current;
+			if (temp != NULL)
+				temp->next = current;
 
-            prev->prev = current;
-            prev->next = current->next;
+			prev->prev = current;
+			prev->next = current->next;
 
-            if (current->next != NULL)
-                current->next->prev = prev;
+			if (current->next != NULL)
+				current->next->prev = prev;
 
-            current->prev = temp;
-            current->next = prev;
+			current->prev = temp;
+			current->next = prev;
 
-            if (temp == NULL)
-                *list = current;
-            else
-                temp->next = current;
+			if (temp == NULL)
+				*list = current;
+			else
+				temp->next = current;
 
-            print_list(*list);
-            prev = current->prev;
-        }
-        current = current->next;
-    }
+			print_list(*list);
+			prev = current->prev;
+		}
+		current = current->next;
+	}
 }
